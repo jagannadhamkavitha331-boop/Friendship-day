567e9dc9c89620719b37b56d0e0deee0dcf7f700
jagannadhamkavitha331-boop <jagannadhamkavitha331@gmail.com>
2026-08-02 00:38:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,159 @@
 let questions = [
-    {
-        q: "Mana friendship lo ekkuva drama evaru chestharu? 😂",
-        options: ["Nenu 🤭", "Nuvvu 😂"]
-    },
-    {
-        q: "Nuvvu nannu emani pilusthav? 😄",
-        options: ["Golduu ❤️", "Bestie 😊"]
-    },
-    {
-        q: "Nannu first time chusinappudu em anukunnav? 😊",
-        options: ["Manchi person ❤️", "Chala crazy 😂"]
-    },
-    {
-        q: "Mana friendship lo funniest memory enti? 😂",
-        options: ["Crazy moments 😜", "Navvina moments ❤️"]
-    },
-    {
-        q: "Naa lo neeku nachina quality enti? ✨",
-        options: ["Caring ❤️",
+{
+q:"Mana friendship lo ekkuva drama evaru chestharu? 😂",
+options:["Nenu 🤭","Nuvvu 😂"]
+},
+{
+q:"Nuvvu nannu emani pilusthav? 😄",
+options:["Golduu ❤️","Bestie 😊"]
+},
+{
+q:"Nannu first time chusinappudu em anukunnav? 😊",
+options:["Manchi person ❤️","Chala crazy 😂"]
+},
+{
+q:"Mana friendship lo funniest memory enti? 😂",
+options:["Crazy moments 😜","Navvina moments ❤️"]
+},
+{
+q:"Naa lo neeku nachina quality enti? ✨",
+options:["Caring ❤️","Funny 😂"]
+},
+{
+q:"Mana friendship ki rating entha? ⭐",
+options:["10/10 ❤️","Infinity ♾️"]
+},
+{
+q:"Future lo kuda ilane friends ga untama? 🤝",
+options:["Always ❤️","Forever ♾️"]
+}
+];
+
+
+let current = 0;
+
+
+function startQuestions(){
+
+document.getElementById("questionBox").style.display="none";
+
+document.getElementById("quizBox").style.display="block";
+
+showQuestion();
+
+}
+
+
+function noAnswer(){
+
+alert("Try again 😄");
+
+}
+
+
+function showQuestion(){
+
+document.getElementById("questionText").innerHTML =
+questions[current].q;
+
+document.getElementById("option1").innerHTML =
+questions[current].options[0];
+
+document.getElementById("option2").innerHTML =
+questions[current].options[1];
+
+}
+
+
+
+function nextQuestion(){
+
+current++;
+
+if(current < questions.length){
+
+showQuestion();
+
+}
+else{
+
+document.getElementById("quizBox").style.display="none";
+
+document.getElementById("messageBox").style.display="block";
+
+}
+
+}
+
+
+
+let photoIndex = 0;
+
+
+let photos = [
+"Snapchat-181886413.jpg",
+"IMG_9919.JPG",
+"IMG_20250328_152814.jpg",
+"IMG_20250328_143737.jpg",
+"IMG_1610.JPEG",
+"IMG_1609.JPEG"
+];
+
+
+let captions = [
+"Beautiful memories ❤️",
+"Always remember these moments ✨",
+"Crazy friendship days 😂",
+"Special moments 🤍",
+"Forever friendship 🤝",
+"Keep smiling 😊"
+];
+
+
+
+function showPhotos(){
+
+document.getElementById("messageBox").style.display="none";
+
+document.getElementById("photoBox").style.display="block";
+
+changePhoto();
+
+}
+
+
+
+function changePhoto(){
+
+let img=document.getElementById("photo");
+
+let text=document.getElementById("caption");
+
+
+img.style.opacity=0;
+
+
+setTimeout(function(){
+
+img.src=photos[photoIndex];
+
+text.innerHTML=captions[photoIndex];
+
+img.style.opacity=1;
+
+
+photoIndex++;
+
+if(photoIndex>=photos.length){
+
+photoIndex=0;
+
+}
+
+
+},1000);
+
+
+setTimeout(changePhoto,5000);
+
+}
