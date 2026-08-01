f4074c23e8b700aeb0ffc805b195e4e8d90433c2
jagannadhamkavitha331-boop <jagannadhamkavitha331@gmail.com>
2026-08-01 23:13:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("Welcome Eswar ❤️");
+let current = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide() {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    slides[current].classList.add("active");
+
+    current++;
+
+    if (current >= slides.length) {
+        current = 0;
+    }
+}
+
+showSlide();
+
+setInterval(showSlide, 4000);
