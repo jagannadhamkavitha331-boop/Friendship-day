410dcea557774c1d0d2840666cdaffd46230955e
jagannadhamkavitha331-boop <jagannadhamkavitha331@gmail.com>
2026-08-02 01:21:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 let questions = [
-    {
-        q: "Mana friendship lo ekkuva drama evaru chestharu? 😂",
-        options: ["Nenu 🤭", "Nuvvu 😂"]
-    },
-    {
-        q: "Nuvvu nannu emani pilusthav? 😄",
-        options: ["Golduu ❤️", "Bestie 😊"]
-    },
-    {
-        q: "Nannu first time chusinappudu em anukunnav? 😊",
-        options: ["Manchi person ❤️", "Chala crazy 😂"]
-    },
-    {
-        q: "Mana friendship lo funniest memory enti? 😂",
-        options: ["Crazy moments 😜", "Navvina moments ❤️"]
-    },
-    {
-        q: "Naa lo neeku nachina quality enti? ✨",
-        options: ["Caring ❤️", "Funny 😂"]
-    },
-    {
-        q: "Mana friendship ki rating entha? ⭐",
-        options: ["10/10 ❤️", "Infinity ♾️"]
-    },
-    {
-        q: "Future lo kuda ilane friends ga untama? 🤝",
-        options: ["Always ❤️", "Forever ♾️"]
-    },
-    {
-        q: "Last question... nenu special friend aa? 🥹",
-        options: ["Yes ❤️", "Of course 😍"]
-    }
+{
+q:"Mana friendship lo ekkuva drama evaru chestharu? 😂",
+options:["Nenu 🤭","Nuvvu 😂"]
+},
+{
+q:"Nuvvu nannu emani pilusthav? 😄",
+options:["Golduu ❤️","Bestie 😊"]
+},
+{
+q:"Nannu first time chusinappudu em anukunnav? 😊",
+options:["Manchi person ❤️","Chala crazy 😂"]
+},
+{
+q:"Mana friendship lo funniest memory enti? 😂",
+options:["Crazy moments 😜","Navvina moments ❤️"]
+},
+{
+q:"Naa lo neeku nachina quality enti? ✨",
+options:["Caring ❤️","Funny 😂"]
+},
+{
+q:"Mana friendship ki rating entha? ⭐",
+options:["10/10 ❤️","Infinity ♾️"]
+},
+{
+q:"Future lo kuda ilane friends ga untama? 🤝",
+options:["Always ❤️","Forever ♾️"]
+},
+{
+q:"Last question... nenu special friend aa? 🥹",
+options:["Yes ❤️","Of course 😍"]
+}
 ];
 
 
@@ -39,17 +39,17 @@ let current = 0;
 
 function startQuestions(){
 
-    document.getElementById("questionBox").style.display="none";
-    document.getElementById("quizBox").style.display="block";
+document.getElementById("questionBox").style.display="none";
+document.getElementById("quizBox").style.display="block";
 
-    showQuestion();
+showQuestion();
 
 }
 
 
 function noAnswer(){
 
-    alert("Try again 😄");
+alert("Try again 😄");
 
 }
 
@@ -57,14 +57,14 @@ function noAnswer(){
 
 function showQuestion(){
 
-    document.getElementById("questionText").innerHTML =
-    questions[current].q;
+document.getElementById("questionText").innerHTML =
+questions[current].q;
 
-    document.getElementById("option1").innerHTML =
-    questions[current].options[0];
+document.getElementById("option1").innerHTML =
+questions[current].options[0];
 
-    document.getElementById("option2").innerHTML =
-    questions[current].options[1];
+document.getElementById("option2").innerHTML =
+questions[current].options[1];
 
 }
 
@@ -72,43 +72,35 @@ function showQuestion(){
 
 function nextQuestion(){
 
-    let buttons = document.querySelectorAll("#quizBox button");
+let box=document.getElementById("quizBox");
 
-    buttons.forEach(btn => btn.disabled = true);
-
-
-    let box = document.getElementById("quizBox");
-
-    box.style.opacity="0";
+box.style.opacity="0";
 
 
-    setTimeout(function(){
+setTimeout(()=>{
 
-        current++;
-
-
-        if(current < questions.length){
-
-            showQuestion();
-
-            box.style.opacity="1";
-
-        }
-        else{
-
-            document.getElementById("quizBox").style.display="none";
-
-            document.getElementById("messageBox").style.display="block";
-
-        }
+current++;
 
 
-        buttons.forEach(btn => btn.disabled=false);
+if(current < questions.length){
 
+showQuestion();
 
-    },2500);
+box.style.opacity="1";
 
 }
+else{
+
+document.getElementById("quizBox").style.display="none";
+
+document.getElementById("messageBox").style.display="block";
+
+}
+
+},1500);
+
+}
+
 
 
 
@@ -119,40 +111,76 @@ let photoIndex = 0;
 
 
 let photos = [
-    "1773888361623.jpg"
+"1773888361623.jpg",
+"IMG_9919.JPG",
+"IMG_20250328_152814.jpg",
+"IMG_20250328_143737.jpg",
+"IMG_1610.JPEG",
+"IMG_1609.JPEG",
+"IMG_1608.JPEG"
 ];
 
 
 let captions = [
-    "Beautiful memories ❤️"
+"Beautiful memories ❤️",
+"Every moment was special ✨",
+"Crazy friendship days 😂",
+"Always remember these smiles 😊",
+"Friendship forever 🤝",
+"Never forget these moments ❤️",
+"Forever memories 📸"
 ];
 
 
 
 function showPhotos(){
 
-    document.getElementById("messageBox").style.display="none";
+document.getElementById("messageBox").style.display="none";
 
-    document.getElementById("photoBox").style.display="block";
+document.getElementById("photoBox").style.display="block";
 
-    photoIndex=0;
+photoIndex=0;
 
-    changePhoto();
+changePhoto();
 
 }
 
 
 
+
 function changePhoto(){
 
-    let img=document.getElementById("photo");
+let img=document.getElementById("photo");
 
-    let text=document.getElementById("caption");
+let text=document.getElementById("caption");
 
 
-    img.src = photos[photoIndex];
+img.style.opacity=0;
 
-    text.innerHTML = captions[photoIndex];
 
+setTimeout(()=>{
+
+img.src=photos[photoIndex];
+
+text.innerHTML=captions[photoIndex];
+
+img.style.opacity=1;
+
+
+photoIndex++;
+
+
+if(photoIndex>=photos.length){
+
+photoIndex=0;
+
+}
+
+
+},500);
+
+
+
+setTimeout(changePhoto,4000);
 
 }
